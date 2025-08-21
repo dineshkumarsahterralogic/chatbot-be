822b3c7f90b02497694a719569113d0543d2307b
DineshS <dinesh.sah@fujitsu.com>
2025-08-21 22:40:53 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,32 +3,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const connectionString="mongodb+srv://root:root@cluster0.jg6heoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
 const chatRoutes = require('./routes/chat');
+const messageRoutes = require('./routes/messages')
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const { MongoClient } = require("mongodb");
-
-const client = new MongoClient(process.env.MONGO_URI);
-let db;
-
-async function connectDB() {
-  try {
-    await client.connect();
-    db = client.db("chatbotDB"); // use your DB name
-    console.log("✅ Connected to MongoDB Atlas");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-  }
-}
 
 app.use('/api/chat', chatRoutes);
-
+app.use('api/m',messageRoutes)
 const PORT = 3000;
-app.listen(PORT, () => 
-    {console.log(`Server running on port ${PORT}`);
-    connectDB();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
